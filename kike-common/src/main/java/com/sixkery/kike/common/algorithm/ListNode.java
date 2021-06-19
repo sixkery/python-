95bc33388265eb3ac7ed55bb13032991ef6f67bb
sixkery <sixkery@163.com>
2021-06-19 16:24:50 +0800

    添加两个类

---
+++ kike-common/src/main/java/com/sixkery/kike/common/algorithm/ListNode.java
@@ -0,0 +1,16 @@
+package com.sixkery.kike.common.algorithm;
+
+/**
+ * 链表
+ *
+ * @author sixkery
+ * @date 2021/6/17
+ */
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
