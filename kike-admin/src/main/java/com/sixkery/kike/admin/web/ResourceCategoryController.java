6b8ce4e655ed4602c2aa4966aba6614ae5368547
sixkery <sixkery@163.com>
2021-07-11 11:27:45 +0800

    删除无用代码

--- kike-admin/src/main/java/com/sixkery/kike/admin/web/ResourceCategoryController.java
+++ kike-admin/src/main/java/com/sixkery/kike/admin/web/ResourceCategoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 
 /**
- * 资源控制器
+ * 资源分类控制器
  *
  * @author: sixkery
  * @date:2021/4/5
@@ -26,45 +26,22 @@ public class ResourceCategoryController {
      * @return 资源信息
      */
     @GetMapping("/listAll")
-    public ApiResponses<Object> List() {
+    public ApiResponses<Object> list() {
         return ApiResponses.ok(resourceCategoryService.findAll());
     }
 
-//    /**
-//     * 根据 ID 查询菜单
-//     *
-//     * @param id id
-//     * @return 菜单信息
-//     */
-//    @GetMapping("/{id}")
-//    public ApiResponses<Object> List(@PathVariable Long id) {
-//        return ApiResponses.ok(menuService.findOne(id));
-//    }
-//
-//
     /**
      * 根据 ID 更新资源分类
      *
-     * @param id      id
+     * @param id                  id
      * @param resourceCategoryDto 入参数据
      * @return 更新条数
      */
     @PostMapping("/update/{id}")
     public ApiResponses<Object> update(@PathVariable Long id, @RequestBody ResourceCategoryDto resourceCategoryDto) {
-        return ApiResponses.ok(resourceCategoryService.update(id,resourceCategoryDto));
+        return ApiResponses.ok(resourceCategoryService.update(id, resourceCategoryDto));
     }
-//
-//    /**
-//     * 创建菜单
-//     *
-//     * @param menuDto 入参数据
-//     * @return 新增条数
-//     */
-//    @PostMapping("/create")
-//    public ApiResponses<Object> create(@RequestBody MenuDto menuDto) {
-//        return ApiResponses.ok(menuService.create(menuDto));
-//    }
-//
+
     /**
      * 删除资源分类
      *
